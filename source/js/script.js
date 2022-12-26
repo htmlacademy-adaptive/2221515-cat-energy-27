9f09ef1fba65986c72c6c4bc14de593ea30c96f8
A.Voln <a.voln@list.ru>
2022-12-26 19:03:44 +0300

    add markup

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,12 @@
-let navMain = document.querySelector('.main-nav');
+document.querySelector('body').classList.remove('nojs');
+
+let pageHeader = document.querySelector('.page-header');
 let navToggle = document.querySelector('.main-nav__toggle');
 
-navMain.classList.remove('main-nav--nojs');
-
 navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
+  if (!pageHeader.classList.contains('page-header--nav-open')) {
+    pageHeader.classList.add('page-header--nav-open');
   } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
+    pageHeader.classList.remove('page-header--nav-open');
   }
 });
