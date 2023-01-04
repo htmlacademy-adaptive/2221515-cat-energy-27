69b661f784d344c7c28049b4bbfb4ccd1552a6cb
Keks <keks@htmlacademy.ru>
2023-01-05 00:10:32 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/script.js
@@ -0,0 +1,12 @@
+document.querySelector('body').classList.remove('nojs');
+
+let pageHeader = document.querySelector('.page-header');
+let navToggle = document.querySelector('.page-header__toggle');
+
+navToggle.addEventListener('click', function () {
+  if (!pageHeader.classList.contains('page-header--nav-open')) {
+    pageHeader.classList.add('page-header--nav-open');
+  } else {
+    pageHeader.classList.remove('page-header--nav-open');
+  }
+});
